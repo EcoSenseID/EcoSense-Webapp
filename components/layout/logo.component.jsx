cfe4e86fcb14a471f04a558609e9228293228a9e
KenTandrian <kenricktan11@gmail.com>
2022-05-18 12:52:13 +0700

    Initial commit

---
+++ components/layout/logo.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+
+import classes from './logo.module.css';
+
+const Logo = () => {
+    return (
+        <>
+            <Image className={classes.logopic} src={`/images/EcoSense-logo-png.png`} height={'40px'} width={'40px'} alt={'Logo EcoSense'}></Image>
+            <div className={classes.logo}>EcoSense</div>
+        </>
+    );
+}
+
+export default Logo;
---
+++ components/layout/main-nav.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+
+import Logo from "./logo.component";
+import classes from './main-nav.module.css';
+
+const MainNavigation = () => {
+    return (
+        <header className={classes.header}>
+            <Link href='/'>
+                <a className={classes.logobox}><Logo /></a>
+            </Link>
+            <nav>
+                <ul>
+                    <li><Link href='/'>Home</Link></li>
+                    <li><Link href='/'>About Us</Link></li>
+                    <li><Link href='/'>Media</Link></li>
+                    <li><Link href='/'>Features</Link></li>
+                    <li><Link href='/'>Contact</Link></li>
+                    <li className={classes.login}><Link href='/'>Log In</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default MainNavigation;
